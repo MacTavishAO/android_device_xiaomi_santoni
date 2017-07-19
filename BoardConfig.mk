9417573e51943e1ac7a4f07bbdb30de5ee6d88e1
Ataberk Özen <ataberkozen123@gmail.com>
2017-07-19 19:37:23 +0300

    sa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,8 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_PREBUILT_KERNEL := device/xiaomi/santoni/kernel
+PRODUCT_COPY_FILES += \
+	$(TARGET_PREBUILT_KERNEL):kernel
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
